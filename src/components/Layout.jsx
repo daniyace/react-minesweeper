cebe78087c49f710cea98752f6a2c2cfab8569a6
Angel Ayala <ayalaceja@hotmail.com>
2021-05-24 15:27:09 -0500

    install 1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,17 @@ import Grid from './Grid';
 import '../styles/layout.css';
 
 const Layout = () => {
+  window.addEventListener('beforeinstallprompt', function (e) {
+    // log the platforms provided as options in an install prompt
+    console.log(e.platforms); // e.g., ["web", "android", "windows"]
+    e.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+    }, handleError);
+  });
+  const handleError = (e) => {
+    console.log(e);
+  };
+
   return (
     <div className='main'>
       <Grid />
